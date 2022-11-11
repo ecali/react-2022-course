578b66ce1017fd372c19a883fd1b8936c5b0cfe7
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-11 14:54:59 +0100

    First step - props - component - jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Job salary={9000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Senior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.position}</h1>
+      <h2>{props.company}</h2>
+      <h2>{props.salary}</h2>
+    </div>
+  );
+};
 
 export default App;
