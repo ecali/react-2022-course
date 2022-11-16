f7977faa641e0c3d5442df0ef0b6f96b81cd3f8b
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-16 11:46:54 +0100

    use state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Job salary={9000} position="Senior SDE" company="Amazon" />
-      <Job salary={12000} position="Senior SDE" company="Google" />
-      <Job salary={10000} position="Project Manager" company="Netflix" />
-    </div>
-  );
-}
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.position}</h1>
-      <h2>{props.company}</h2>
-      <h2>{props.salary}</h2>
-    </div>
-  );
-};
+  var [age, setAge] = useState(0);
+
+  const increaseAge = () => {
+    setAge(age + 1);
+  }
+
+  return (<div className="App">
+    {age}
+    <button onClick={increaseAge}>Increase Age</button>
+  </div>);
+}
 
 export default App;
