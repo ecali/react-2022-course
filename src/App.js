773e34e90698988163b503e4ff73b77af7f9ba13
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-16 12:41:19 +0100

    exercise

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [textColor, setTextColor] = useState('black');
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <button onClick={() => setTextColor( textColor === 'red' ? 'black' : 'red')}>Show/Hide</button>
-      <h1 style={{color: textColor}}>HI MY NAME IS PEDRO</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Set to Zero</button>
+
+      {count}
     </div>
   );
 }
