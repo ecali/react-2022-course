756306faaa0123cafd827621308308d622e5816b
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-17 20:05:28 +0100

    use effect

---
+++ src/Text.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export const Text = () => {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("COMPONENT MOUNTED");
+    return () => {
+      console.log("COMPONEND DISMOUNT");
+    };
+  }, []);
+
+  return (
+    <div>
+      <input
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
+      <h1>{text}</h1>
+    </div>
+  );
+};
