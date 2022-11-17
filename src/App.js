b173aae94da845e9f23b113b4a95dc48b9cc6901
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-17 20:46:51 +0100

    fetch data from api and useState

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import "./App.css";
 import Axios from "axios";
 import { useEffect, useState } from "react";
 function App() {
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
 
-  const [catFact, setCatFact] = useState("");
-
-  const fetchCatFact = () => {
-    Axios.get('https://catfact.ninja/fact').then((res) => {
-      setCatFact(res.data.fact);
+  const fecthData = () => {
+    Axios.get("https://api.agify.io/?name=" + name).then((res) => {
+      setPredictedAge(res.data);
     });
   };
 
-  useEffect( () => {
-    fetchCatFact();
-  }, []);
-
   return (
     <div className="App">
-      <button onClick={fetchCatFact} >Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <input
+        placeholder="Ex. Pedro..."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fecthData} disabled={!name}>
+        Predict Age
+      </button>
+      {predictedAge ? (
+        <div>
+          <h1>Name: {predictedAge.name} </h1>
+          <h1>Predicted Age: {predictedAge.age} </h1>
+          <h1>Count: {predictedAge.count} </h1>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
