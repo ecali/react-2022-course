244d48d456b2c5f4bd54f01100327e46566b9530
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-18 10:26:41 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Navbar } from "./Navbar";
+import { Contact } from "./pages/Contact";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
