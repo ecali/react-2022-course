77502b295c1953fe2606dcaded45663174eb0a5e
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-21 14:52:07 +0100

    simle custom hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import { useToggle } from "./useToggle";
 
 function App() {
+
+  const [isVisible, setIsVisible] = useToggle();
+
   return (
     <div className="App">
+      <button onClick={setIsVisible}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
+      {isVisible && <h1>Hidden Text</h1>}
     </div>
   );
 }
