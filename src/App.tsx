d48c71956d16a700082ef2d29ae8725c07e48b06
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-11-22 11:20:21 +0100

    interface and enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-
-import './App.css';
+import "./App.css";
+import { Country, Person } from "./components/Person";
 
 function App() {
   return (
     <div className="App">
-
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["jessica", "jake", "jerry", "jasmine"]}
+        country = {Country.Brazil}
+      />
     </div>
   );
 }
